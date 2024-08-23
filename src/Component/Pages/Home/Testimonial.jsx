c39e9245556b793f6fbeaee90eba0ecc5ba7d494
darshak kanani <kdarshak2910@gmail.com>
2024-08-23 13:40:03 +0530

    testimonial pagination color change

--- src/Component/Pages/Home/Testimonial.jsx
+++ src/Component/Pages/Home/Testimonial.jsx
@@ -19,10 +19,10 @@ export default function Testimonial() {
                     }}
                     loop={true}
                     modules={[Autoplay, Pagination, Navigation]}
-                    autoplay={{
-                        delay: 2000,
-                        disableOnInteraction: false,
-                    }}
+                    // autoplay={{
+                    //     delay: 2000,
+                    //     disableOnInteraction: false,
+                    // }}
                     navigation={{
                         nextEl: ".swiper-button-next",
                         prevEl: ".swiper-button-prev",
