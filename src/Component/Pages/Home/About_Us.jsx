9e80ea650f4e61282419cd8395729022a6d8a315
darshak kanani <kdarshak2910@gmail.com>
2024-08-24 13:30:00 +0530

    create lets talk section

--- src/Component/Pages/Home/About_Us.jsx
+++ src/Component/Pages/Home/About_Us.jsx
@@ -1222,6 +1222,67 @@ export default function About_Us() {
                     </div>
                 </div>
             </div>
+
+            {/* Lets Talk Section */}
+            <div>
+                <div className='lets-contact-section'>
+                    <div class="contact-section">
+                        <div class="container-lg container-fluid">
+                            <div class="row g-lg-4 gy-5">
+                                <div class="col-lg-8">
+                                    <div class="section-title text-animation vector-and-review">
+                                        <div className='lest-talk-sec-style-1'>
+                                            <h2>Let’s <span>TALK</span></h2>
+                                        </div>
+                                        <div className='lest-talk-sec-style-2'>
+                                            <div class="dash-and-paragraph">
+                                                <div class="dash"></div>
+                                                <div class="content-and-social">
+                                                    <p className='vector-and-review'>
+                                                        <div className='lest-talk-sec-style-3'>Crafting a contact text for a digital agency
+                                                        </div>
+                                                        <div className='lest-talk-sec-style-3'>involvesproviding esential information for potential
+                                                        </div>
+                                                        <div className='lest-talk-sec-style-3'>clients or collaborators to reach out.</div>
+                                                    </p>
+                                                    <div class="social-area">
+                                                        <h6>Connect Us</h6>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="6" viewBox="0 0 50 6">
+                                                            <path d="M50 3L45 0.113249V5.88675L50 3ZM0 3.5H45.5V2.5H0V3.5Z"></path>
+                                                        </svg>
+                                                        <ul>
+                                                            <li><a href="#">
+                                                                <svg aria-hidden="true" class="e-font-icon-svg e-fab-facebook-f" viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg></a>
+                                                            </li>
+                                                            <li><a href="#">
+                                                                <svg aria-hidden="true" class="e-font-icon-svg e-fab-x-twitter" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path></svg></a>
+                                                            </li>
+                                                            <li><a href="#">
+                                                                <svg aria-hidden="true" class="e-font-icon-svg e-fab-linkedin-in" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg></a>
+                                                            </li>
+                                                            <li><a href="#">
+                                                                <svg aria-hidden="true" class="e-font-icon-svg e-fab-instagram" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg></a>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div></div></div>
+                                </div>
+                                <div class="col-lg-4 d-lg-flex justify-content-end align-items-center">
+                                    <div class="btn_wrapper lets-talk-btn-style">
+                                        <a class="circle-btn btn-hover" href="https://axleo-wp.egenslab.com/contact-us/" target="_blank" rel="nofollow">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12">
+                                                <path d="M10.0035 3.40804L1.41153 12L0 10.5885L8.59097 1.99651H1.01922V0H12V10.9808H10.0035V3.40804Z">
+                                                </path>
+                                            </svg> Contact <strong>Now</strong> <span></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
