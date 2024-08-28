c59bf8ba4182a7afc496dc8358a91561dbb31f55
darshak kanani <kdarshak2910@gmail.com>
2024-08-28 13:30:00 +0530

    final changes

--- src/Component/Pages/Home/About_Us.jsx
+++ src/Component/Pages/Home/About_Us.jsx
@@ -316,48 +316,6 @@ export default function About_Us() {
 
             {/* Exper and projects */}
             <Project></Project>
-            {/* <div className='expert-section'>
-                <div className="home2-counter-area">
-                    <div className="container-lg container-fluid">
-                        <div className="row justify-content-lg-center justify-content-center">
-                            <div className="col-lg-10 col-md-12 col-12">
-                                <div className="row g-4">
-                                    <div className="col-lg-4">
-                                        <div className="single-counter py-lg-0 py-md-3 py-3 mb-0">
-                                            <div className="content">
-                                                <div className="number d-flex justify-content-center">
-                                                    <h2 className="counter">34</h2>
-                                                </div>
-                                                <p>Professional Experts</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-4">
-                                        <div className="single-counter py-lg-0 py-md-3 py-3 mb-0">
-                                            <div className="content">
-                                                <div className="number d-flex justify-content-center">
-                                                    <h2 className="counter">145</h2>
-                                                </div>
-                                                <p>Completed Projects</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-4">
-                                        <div className="single-counter py-lg-0 py-md-3 py-3 mb-0">
-                                            <div className="content">
-                                                <div className="number d-flex justify-content-center">
-                                                    <h2 className="counter">5</h2>
-                                                </div>
-                                                <p>Year of Experienece</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
 
             {/* inovative idea section */}
             <div>
