270fe3d1375ea55781c8edb74947d14f9c298330
darshak kanani <kdarshak2910@gmail.com>
2024-09-03 13:04:46 +0530

    change footer mob no

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -165,7 +165,7 @@ export default function Footer() {
                                                             <ul className="footer-contact">
                                                                 <li className="single-contact">
                                                                     <span>Phone</span>
-                                                                    <h5><a href="tel:9917636844563">+991 - 763 684 4563 </a></h5>
+                                                                    <h5><a href="tel:919099310347">+91 - 9099310347 </a></h5>
                                                                 </li>
                                                                 <li className="single-contact">
                                                                     <span>Email Now</span>
